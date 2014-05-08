347cb0cd4ced3bcec6ead7bd78aa02cb331c3e4c
viniels <vincent.geers@zonnet.nl>
2014-05-08 09:44:14 +0200

    start created for project

---
+++ Project Pacman/src/project/pacman/Game.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package project.pacman;
+
+/**
+ *
+ * @author Vincent
+ */
+public class Game {
+    
+}
---
+++ Project Pacman/src/project/pacman/ProjectPacman.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package project.pacman;
+
+/**
+ *
+ * @author Vincent
+ */
+public class ProjectPacman {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
