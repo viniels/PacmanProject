6773a867ca8a5d951a9ce0b860e0353140f3fc2d
viniels <vincent.geers@zonnet.nl>
2014-05-15 13:23:20 +0200

    WOOOT

---
+++ Workshop 4 blok D/src/workshop/pkg4/blok/d/Workshop4BlokD.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package workshop.pkg4.blok.d;
+
+/**
+ *
+ * @author Vincent
+ */
+public class Workshop4BlokD {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
